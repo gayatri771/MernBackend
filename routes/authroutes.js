69a8f1c667c0538b8398f71c54923d8ad43be334
gayatri771 <gayatri94707@gmail.com>
2026-07-23 21:16:15 +0530

    Update authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const {register, login} = require('../Controllers/authcontroller')
+const {register, login} = require('../controllers/authcontroller')
 let router = Router();
 router.post('/register', register)
 router.post('/login', login)
