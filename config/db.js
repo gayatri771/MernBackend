8f1e2adb7ad1e0c6e18e79027bedac5c44de0086
gayatri771 <gayatri94707@gmail.com>
2026-07-23 21:17:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose=require('mongoose');
 const dotenv=require('dotenv').config();
 async function connection(){
     try {
-      console.log(process.env.MONGO_URI); 
       await mongoose.connect(process.env.MONGO_URI)
       console.log('db connected')
     } catch (error) {
